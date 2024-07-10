9c282e0bfc49d496c9957949afa616ee5ae9fbe8
fernando hazan vieira <fernandohvk4@gmail.com>
2024-07-10 11:29:42 -0300

    Versão 2.0
    Historico adicionado

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function exibir(gasto, lucro){
     caixa1.classList.remove('display__none')
     caixa1.classList.add('corpo__principal__resultado')
 
-    if(lucro > 0 || lucro < 0){
+    if(lucro >= 0 || lucro < 0){
     const caixa2 = document.querySelector(".java2")
     caixa2.classList.remove('display__none')
     caixa2.classList.add('corpo__principal__resultado')
@@ -89,8 +89,38 @@ function historico(distancia, valorDoCombustivel, consumoPorkm, valorDoFrete, ga
 }
 
 function exibirHistorico(){
+    
+    
+    let dados = document.getElementById('caixa1')
+    
+    for(let i=0; i < arrayTeste.length; i++){
 
+      if(arrayTeste[i].valorDoFrete >= 0 || arrayTeste[i].valorDoFrete <= 0){
+        dados.innerHTML = dados.innerHTML + `<div class="corpo__principal__caixa-historico__bloco1"><p>Distancia: $${arrayTeste[i].distancia}</p><p>Preço por litro: $${arrayTeste[i].consumoPorkm}</p><p>Preço do combustivel: $${arrayTeste[i].valorDoCombustivel}</p><p>Preço do frete: $${arrayTeste[i].valorDoFrete}</p><div class="corpo__principal__caixa-historico__bloco2"><p class="corpo__principal__caixa-historico__bloco2__valores">Lucro: $${arrayTeste[i].lucro}</p><p class="corpo__principal__caixa-historico__bloco2__valores">Gasto: $${arrayTeste[i].gasto}</p></div></div>`
+    } else{
+        dados.innerHTML = dados.innerHTML + `<div class="corpo__principal__caixa-historico__bloco1"><p>Distancia: $${arrayTeste[i].distancia}</p><p>Preço por litro: $${arrayTeste[i].consumoPorkm}</p><p>Preço do combustivel: $${arrayTeste[i].valorDoCombustivel}</p><p>Preço do frete: xx</p><div class="corpo__principal__caixa-historico__bloco2"><p class="corpo__principal__caixa-historico__bloco2__valores">Lucro: xx</p><p class="corpo__principal__caixa-historico__bloco2__valores">Gasto: $${arrayTeste[i].gasto}</p></div></div>`
+    }
 }
     
+    const caixa1 = document.querySelector(".troca1")
+    const caixa2 = document.querySelector(".troca2")
+    if(caixa1.classList.contains("display__none")){
+    
+        caixa1.classList.remove('display__none')
+        caixa1.classList.add('corpo__principal__historico-titulo')
+
+        caixa2.classList.remove('display__none')
+        caixa2.classList.add('corpo__principal__caixa-historico')
 
 
+    } else{
+    
+        caixa1.classList.add('display__none')
+        caixa1.classList.remove('corpo__principal__historico-titulo')
+
+        caixa2.classList.add('display__none')
+        caixa2.classList.remove('corpo__principal__caixa-historico')
+
+        dados.innerHTML = ''
+    }
+}
